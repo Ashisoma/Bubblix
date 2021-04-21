54ecd4a09311434af4b391f449c66dac0a113ff4
Ashisoma <okalionesmus@gmail.com>
2021-04-22 00:43:33 +0300

    firebase intergration and animation

--- app/src/main/java/com/example/bubblix/helper/UserHelper.java
+++ app/src/main/java/com/example/bubblix/helper/UserHelper.java
@@ -1,5 +1,14 @@
 package com.example.bubblix.helper;
 
 public class UserHelper {
+    String name, email, location;
 
+    public UserHelper() {
+    }
+
+    public UserHelper(String name, String email, String location) {
+        this.name = name;
+        this.email = email;
+        this.location = location;
+    }
 }
--- app/src/main/java/com/example/bubblix/ui/LogIn.java
+++ app/src/main/java/com/example/bubblix/ui/LogIn.java
@@ -1,16 +1,126 @@
 package com.example.bubblix.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.bubblix.MainActivity;
 import com.example.bubblix.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class LogIn extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LogIn extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.registerTextView) TextView registerText;
+    //    @BindView(R.id.logIn)
+//    TextView mLoginButton;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.forgotTextView) TextView mForgotTextView;
+    @BindView(R.id.passwordLoginButton) Button mLoginBtn;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private ProgressDialog mAuthProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        ButterKnife.bind(this);
+
+        registerText.setOnClickListener(this);
+//        mLoginButton.setOnClickListener(this);
+        mForgotTextView.setOnClickListener(this);
+        mLoginBtn.setOnClickListener(this);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Intent intent = new Intent(LogIn.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        createAuthProgressDialog();
+    }
+
+    private void createAuthProgressDialog(){
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    public void onClick(View v) {
+
+        if(v == registerText) {
+            Intent intent = new Intent(LogIn.this, CreateAccount.class);
+            startActivity(intent);
+//            finish();
+        }
+        if (v == mForgotTextView){
+            Intent intent = new Intent(LogIn.this, ForgotPasswprd.class);
+            startActivity(intent);
+        }
+        if ( v== mLoginBtn ){
+            loginWithPassword();
+
+        }
+    }
+
+    private void loginWithPassword(){
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            mEmailEditText.setError("Please enter a valid email address");
+            return;
+        }
+        else if(TextUtils.isEmpty(password)){
+            mPasswordEditText.setError("Please enter password");
+            return;
+        }
+        else if(password.length()>6){
+            mPasswordEditText.setError("Please create a password containing at least 6 characters");
+            return;
+        }
+
+        mAuthProgressDialog.show();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                mAuthProgressDialog.dismiss();
+                if (task.isSuccessful()) {
+                    Toast.makeText(LogIn.this, "Authentication successful.", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LogIn.this, DashboardActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+                mAuthProgressDialog.dismiss();
+            }
+        });
     }
 }
