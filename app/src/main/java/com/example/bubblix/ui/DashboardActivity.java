98adbd63b93dc9f75d2153067f7326fb5ee3a8eb
Ashisoma <okalionesmus@gmail.com>
2021-04-25 16:20:12 +0300

    add drawer menu

--- app/src/main/java/com/example/bubblix/ui/DashboardActivity.java
+++ app/src/main/java/com/example/bubblix/ui/DashboardActivity.java
@@ -1,16 +1,89 @@
 package com.example.bubblix.ui;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.bubblix.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    //Variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    Menu menu;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        // textView=findViewById(R.id.textView);
+        toolbar = findViewById(R.id.toolbar);
+
+        /// toolbar
+
+        setSupportActionBar(toolbar);
+
+        // navigation
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+//        navigationView.setCheckedItem(R.id.nav_home);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                Intent intent = new Intent(DashboardActivity.this, DashboardActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_profile:
+                Intent intent2 = new Intent(DashboardActivity.this, ProfileActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.nav_cart:
+                Intent i = new Intent(DashboardActivity.this, CartActivity.class);
+                startActivity(i);
+                break;
+            case R.id.logOut:
+                Intent intent3 = new Intent(DashboardActivity.this, LogIn.class);
+                startActivity(intent3);
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
